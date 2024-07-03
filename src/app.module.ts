c23f29f7bfce1e83193bf8fa98cfafbc1447256f
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-07-03 15:07:40 +0900

    JWT 인증 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+//데이터베이스와 환경 설정을 안전하고 효율적으로 관리하기 위한 기초 제공
 import Joi from 'joi';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-
+//NestJS Module 관련
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+//TypeOrmModule: TypeORM을 사용하여 데이터베이스 연결 및 ORM 기능을 NestJS에 통합하는 모듈.
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 const typeOrmModuleOptions = {
   useFactory: async (
@@ -16,8 +20,11 @@ const typeOrmModuleOptions = {
     host: configService.get('DB_HOST'),
     port: configService.get('DB_PORT'),
     database: configService.get('DB_NAME'),
+    //'entities' 데이터베이스 테이블과 매핑될 엔티티 클래스들을 배열로 명시
     entities: [],
+    //'synchronize' 엔티티 정보를 바탕으로 데이터베이스 스키마를 자동으로 동기화할지 여부를 결정
     synchronize: configService.get('DB_SYNC'),
+    //데이터베이스 작업 로그 활성화
     logging: true,
   }),
   inject: [ConfigService],
@@ -25,6 +32,7 @@ const typeOrmModuleOptions = {
 
 @Module({
   imports: [
+    //forRoot 메서드를 사용하여 환경 변수를 애플리케이션 전역에서 사용할 수 있도록 설정
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
@@ -38,7 +46,10 @@ const typeOrmModuleOptions = {
       }),
     }),
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    AuthModule,
+    UserModule,
   ],
+  //애플리케이션의 컨트롤러와 서비스(프로바이더)를 여기에 추가할 수 있음
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  //AppModule을 사용하여 Nest 애플리케이션 인스턴스를 비동기적으로 생성
   const app = await NestFactory.create(AppModule);
 
+  //애플리케이션의 모든 컨트롤러와 라우트에 대해 유효성 검사 파이프가 작동하게 함
   app.useGlobalPipes(
+    //요청에서 받은 데이터를 DTO에 선언된 타입으로 자동 변환.
     new ValidationPipe({
       transform: true,
     }),
   );
 
+  //NestJS 서버가 3000 포트에서 수신 대기하도록 설정
   await app.listen(3000);
 }
 
