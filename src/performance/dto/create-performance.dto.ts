80cf5de289b16facc5d6d011d5a839aa825820af
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-07-04 17:30:35 +0900

    update. 공연 날짜 시간별 여러개 생성 기능 추가

--- src/performance/dto/create-performance.dto.ts
+++ src/performance/dto/create-performance.dto.ts
@@ -1,5 +1,22 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsArray, IsDate, IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Category } from '../types/performanceCategory.type';
+import { Type } from 'class-transformer'
+
+class PerformanceScheduleDto {
+    @IsDate()
+    @Type(() => Date)
+    @IsNotEmpty({ message: '공연 날짜를 입력해주세요.' })
+    performanceDate: Date;
+
+    @IsString()
+    @IsNotEmpty({ message: '공연 시간을 입력해주세요.' })
+    performanceTime: string;
+
+    @IsNumber()
+    @IsNotEmpty({ message: '전체 좌석수를 넣어주세요.' })
+    totalSeats:number;
+
+}
 
 export class CreatePerformanceDto {
   @IsString()
@@ -26,16 +43,8 @@ export class CreatePerformanceDto {
   @IsNotEmpty({ message: '이미지를 넣어주세요.' })
   img: string;
 
-  @IsString()
-  @IsNotEmpty({ message: '공연 날짜를 입력해주세요.' })
-  performanceDate: string;
-
-  @IsString()
-  @IsNotEmpty({ message: '공연 시간을 입력해주세요.' })
-  performanceTime: string;
-
-  @IsNumber()
-  @IsNotEmpty({ message: '전체 좌석수를 넣어주세요.' })
-  totalSeats: number;
+  @IsArray()
+  @Type(() => PerformanceScheduleDto)
+  schedules: PerformanceScheduleDto[]
 
 }
--- src/performance/performance.service.ts
+++ src/performance/performance.service.ts
@@ -26,6 +26,9 @@ export class PerformanceService {
   async findAll(): Promise<Performance[]> {
     return await this.performanceRepository.find({
       select: ['performanceId', 'title', 'category', 'location', 'price', 'createdAt', 'updatedAt'],
+      order: {
+        createdAt: 'DESC'
+      }
     });
   }
 
@@ -39,18 +42,20 @@ export class PerformanceService {
 
   //공연 등록
   async create(createPerformanceDto: CreatePerformanceDto): Promise<Performance> {
-    const { title, description, category, location, price, performanceDate, performanceTime, totalSeats, img } = createPerformanceDto;
+    const { title, description, category, location, price, img, schedules } = createPerformanceDto;
     
     const performance = this.performanceRepository.create({
         title, description, category, location, price, img });
 
     const savedPerformance = await this.performanceRepository.save(performance);
 
-    const schedule = this.performanceScheduleRepository.create({
-      performanceDate, performanceTime, totalSeats, remainingSeats: totalSeats, performance: savedPerformance  // 관계 설정
-    })
-
-    await this.performanceScheduleRepository.save(schedule);
+    const performanceSchedule = schedules.map(schedule =>
+        this.performanceScheduleRepository.create({
+            ...schedule,
+            performance: savedPerformance,
+        })
+        )
+       await this.performanceScheduleRepository.save(performanceSchedule);
 
     return savedPerformance;
     }
